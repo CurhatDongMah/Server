0b75b2b6f747f9de4d5d6d82615b1d7c0faa5159
heyhadi <hadi.munawirul@gmail.com>
2021-02-19 14:55:10 +0700

    add model validation

--- models/therapist.js
+++ models/therapist.js
@@ -21,17 +21,91 @@ module.exports = (sequelize, DataTypes) => {
     }
   };
   Therapist.init({
-    fullName: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    photoUrl: DataTypes.STRING,
-    birthDate: DataTypes.DATE,
-    gender: DataTypes.STRING,
-    city: DataTypes.STRING,
-    licenseUrl: DataTypes.STRING,
-    price: DataTypes.INTEGER,
+    fullName: {
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty:{
+          msg: 'fullName is required'
+        }
+      }
+    },
+    email: {
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty:{
+          msg: 'email is required'
+        }
+      }
+    },
+    password: {
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty:{
+          msg: 'password is required'
+        }
+      }
+    },
+    photoUrl: {
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty:{
+          msg: 'photoUrl is required'
+        }
+      }
+    },
+    birthDate: {
+      type:DataTypes.DATE,
+      validate: {
+        notEmpty:{
+          msg: 'birthDate is required'
+        }
+      }
+    },
+    gender: {
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty:{
+          msg: 'gender is required'
+        }
+      }
+    },
+    city: {
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty:{
+          msg: 'city is required'
+        }
+      }
+    },
+    licenseUrl: {
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty:{
+          msg: 'licenseUrl is required'
+        }
+      }
+    },
+    price: {
+      type:DataTypes.INTEGER,
+      validate: {
+        min:{
+          args: 0,
+          msg:"Minimum price is 0"
+        },
+        isNumeric: {
+          msg:'Price must be a number'
+        }
+      }
+    },
     status: DataTypes.BOOLEAN,
-    about: DataTypes.STRING,
+    about: {
+      type:DataTypes.STRING,
+      validate: {
+        notEmpty:{
+          msg: 'about is required'
+        }
+      }
+    },
     rating: DataTypes.INTEGER
   }, {
     hooks: {
